5fce7fa0aeb83e574ee378af55292817d635b5b0
Desmond Tuiyot '20 <dtuiyot@colgate.edu>
2020-01-24 12:12:20 -0500

    added some operations, declarations, etc

--- activity 1/activity1_script.R
+++ activity 1/activity1_script.R
@@ -1,1 +1,28 @@
 print("Hello R World")
+
+# multiply 3 by 3
+3*3
+
+# multiply 6 by 6
+6^6
+
+# create a numerical object with value 2446, called 'a'
+a<-2446
+
+# divide a by 2
+a/2
+
+# create vector called c, with the values below
+b<-c(2446, 40, 10000)
+
+# create vector c, with values below
+c<-c(1,2,3,4)
+
+# add b and c
+b+c
+
+# b*3
+b*3
+
+# 
+
